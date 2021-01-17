225138cf05d8a27827e63335b40d1d811f97eeaf
cu4nt0m <cu4nt0m@gmail.com>
2021-01-17 16:48:39 +0200

    added OpenConversation.js for now

---
+++ src/components/OpenConversation.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Button, Form, InputGroup } from 'react-bootstrap';
+import { useConversations } from '../contexts/ConversationsProvider';
+
+const OpenConversation = () => {
+    const [text, setText] = useState('');
+    const { sendMessage, selectedConversation } = useConversations();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        sendMessage(selectedConversation.recipients.map(r => r.id), text)  
+    }
+
+    return (
+        <div className="d-flex flex-column flex-grow-1">
+            <div className="flex-grow-1 overflow-auto">
+
+            </div>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="m-2">
+                        <InputGroup>
+                        <Form.Control 
+                            as="textarea"
+                            required
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                            style={{ height: '75px', resize: 'none'}}/>
+                            <InputGroup.Append>
+                                <Button type="submit">Send</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
+                    </Form.Group>
+                </Form>
+            
+        </div>
+    )
+}
+
+export default OpenConversation
