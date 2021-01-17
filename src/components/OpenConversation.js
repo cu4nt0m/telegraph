a197fb15bb68afcde083427e2112232155bb73e6
cu4nt0m <cu4nt0m@gmail.com>
2021-01-17 19:45:20 +0200

    [client] Message sending to individual conversations done!

--- src/components/OpenConversation.js
+++ src/components/OpenConversation.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { Button, Form, InputGroup } from 'react-bootstrap';
 import { useConversations } from '../contexts/ConversationsProvider';
 
 const OpenConversation = () => {
     const [text, setText] = useState('');
     const { sendMessage, selectedConversation } = useConversations();
+    const setRef = useCallback(node => {
+        if (node){
+            node.scrollIntoView({smooth: true})
+        }
+    }, [])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -14,10 +19,30 @@ const OpenConversation = () => {
         setText('');
     }
 
+
     return (
         <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
-
+                <div className="d-flex flex-column align-items-start justify-content-end px-3">
+                    {selectedConversation.messages.map((message,index) => {
+                        const lastMessage = selectedConversation.messages.length - 1 === index
+                        return (
+                            <div 
+                                ref={lastMessage ? setRef : null}
+                                key={index}
+                                className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end' : ''}`}>
+                                <div 
+                                    className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border'}`}>
+                                    {message.text}
+                                </div>
+                                <div 
+                                    className={`text-muted small ${message.fromMe ? 'text-right' : ''}`}>
+                                    {message.fromMe ? 'You' : message.senderName}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="m-2">
--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -19,18 +19,6 @@ export const ConversationsProvider = ({id, children}) => {
         });
     };
 
-    const formattedConversations = conversations.map((conversation, index) => {
-        const recipients = conversation.recipients.map(recipient => {
-            const contact = contacts.find(contact => {
-                return contact.id === recipient;
-            });
-            const name = (contact && contact.name) || recipient;
-            return {id: recipient, name};
-        });
-        const selected = index === selectedConversationIndex
-        return {...conversation, recipients, selected }
-    })
-
     const addMessageToConversation = ({ recipients, text, sender }) => {
         setConversations(prevConversations => {
             let madeChange = false;
@@ -60,6 +48,28 @@ export const ConversationsProvider = ({id, children}) => {
     const sendMessage = (recipients, text) => {
         addMessageToConversation( {recipients, text, sender: id });
     }
+
+    const formattedConversations = conversations.map((conversation, index) => {
+        const recipients = conversation.recipients.map(recipient => {
+            const contact = contacts.find(contact => {
+                return contact.id === recipient;
+            });
+            const name = (contact && contact.name) || recipient;
+            return {id: recipient, name};
+        });
+        
+        const messages = conversation.messages.map(message => {
+            const contact = contacts.find(contact => {
+                return contact.id === message.sender
+            })
+            const name = (contact && contact.name) || message.sender
+            const fromMe = id === message.sender
+            return {...message, senderName: name, fromMe}
+        })
+
+        const selected = index === selectedConversationIndex;
+        return {...conversation, messages, recipients, selected }
+    })
 
     return (
         <div>
