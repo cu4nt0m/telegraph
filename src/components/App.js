b22b42b2fdd6a9e0891e8282f2eee17c3d3e448b
cu4nt0m <cu4nt0m@gmail.com>
2021-01-19 00:54:12 +0200

    [client] added websockets, polling, flashsocket to io to fix the xhtml blockage on port 5000

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,19 @@ import Login from './Login';
 import Dashbord from './Dashbord';
 import { ContactsProvider } from '../contexts/ContactsProvider';
 import { ConversationsProvider } from '../contexts/ConversationsProvider';
+import { SocketProvider } from '../contexts/SocketProvider';
 
 function App() {
   const [id, setId] = useLocalStorage('id')
 
   const dashboard = (
-    <ContactsProvider>
-      <ConversationsProvider id={id}>
-        <Dashbord id={id} />
-      </ConversationsProvider>
-    </ContactsProvider>
+    <SocketProvider id={id}>
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Dashbord id={id} />
+        </ConversationsProvider>
+      </ContactsProvider>
+    </SocketProvider>
   )
 
   return (
--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState, useCallback } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage';
 import { useContacts } from './ContactsProvider';
+import { useSocket } from './SocketProvider';
 
 const ConversationsContext = React.createContext();
 
@@ -12,6 +13,7 @@ export const ConversationsProvider = ({id, children}) => {
     const [conversations, setConversations] = useLocalStorage('conversations', []);
     const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
     const { contacts } = useContacts();
+    const socket = useSocket();
 
     const createNewConversation = (recipients) => {
         setConversations(prevConversations => {
@@ -19,7 +21,7 @@ export const ConversationsProvider = ({id, children}) => {
         });
     };
 
-    const addMessageToConversation = ({ recipients, text, sender }) => {
+    const addMessageToConversation = useCallback(({ recipients, text, sender }) => {
         setConversations(prevConversations => {
             let madeChange = false;
             const newMessage = { sender, text }
@@ -43,9 +45,18 @@ export const ConversationsProvider = ({id, children}) => {
                 ]
             }
         })
-    }
+    }, [setConversations])
+
+    useEffect(() => {
+        if (socket == null) return
+
+        socket.on('receive-message', addMessageToConversation)
+
+        return () => socket.off('receive-message')
+    },[socket, addMessageToConversation])
 
     const sendMessage = (recipients, text) => {
+        socket.emit('send-message', {recipients, text})
         addMessageToConversation( {recipients, text, sender: id });
     }
 
